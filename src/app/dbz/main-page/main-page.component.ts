c068f6a48117c5fd5303c7f338b86dfc1be41b5b
Agustin Farisano <farisanoagustin@gmail.com>
2022-09-23 14:37:44 -0300

    add personaje interface, array, ngfor and to way data binding for add a new list item

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje{
+  nombre:string;
+  poder:number;
+}
+
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,13 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+  nuevo:Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  personajes:Personaje[]=[
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 5500
+    }
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   agregar(){
-    console.log("prueba!");
+    if(this.nuevo.nombre.trim().length === 0){return}
+    console.log(this.nuevo);
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
   }
-
 }
